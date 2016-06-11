059b3d8853288d203ff25a92c6ea6eed72ef2ee1
boluo250 <169180920@qq.com>
2016-06-11 22:08:20 +0800

    写入一个文件

---
+++ ofstream.cc
@@ -0,0 +1,34 @@
+ ///
+ /// @file    ifstream.cc
+ /// @author  boluo(169180920@qq.com)
+ /// @date    2016-06-11 21:56:02
+ ///
+ 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream ifs("cinignore.cc");
+	if(!ifs.good())
+	{
+		cout << "ifstream open error" << endl;
+		return -1;
+	}
+	ofstream ofs("data.bat");//文件不存在,会自动创建
+	string str;
+	while(getline(ifs,str))
+	{
+		ofs << str << endl;
+	}
+
+	ofs.close();
+	ifs.close();
+	return 0;
+}
+
+
+
